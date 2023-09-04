0c9e2b86fa38ef7f15050956a56ca2ce5e07efa4
wormsJJG <81068345+wormsJJG@users.noreply.github.com>
2023-09-04 11:39:03 +0900

    feat: Project Init

---
+++ Parple/Parple/ParpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParpleApp.swift
+//  Parple
+//
+//  Created by 정재근 on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct ParpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
